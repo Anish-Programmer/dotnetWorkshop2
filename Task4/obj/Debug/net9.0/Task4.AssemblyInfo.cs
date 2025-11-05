6249f0b966300fbbb89438f5ef7a08e286f2f046
Anish-Programmer <at7anish@gmail.com>
2025-11-05 13:29:05 +0545

    task 6 completed

--- Task4/obj/Debug/net9.0/Task4.AssemblyInfo.cs
+++ Task4/obj/Debug/net9.0/Task4.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Task4")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+eaa43ebd66480fd36bfe55d7ce27e5cbc90ac5a9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+044d42505c0b66785138defcc4af93dac6522166")]
 [assembly: System.Reflection.AssemblyProductAttribute("Task4")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Task4")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ Task6/Program.cs
@@ -0,0 +1,33 @@
+﻿// 1. Create a List<string> with 3 favorite fruits
+List<string> fruits = new List<string>() { "Mango", "Apple", "Banana" };
+
+// 2. Add a new fruit
+fruits.Add("Orange");
+
+// 3. Remove one fruit
+fruits.Remove("Apple");
+
+// 4. Print all fruits using foreach
+Console.WriteLine("Fruits in the List:");
+foreach (string fruit in fruits)
+{
+    Console.WriteLine(fruit);
+}
+
+// 5. Create a Dictionary<int, string> with fruit IDs
+Dictionary<int, string> fruitDict = new Dictionary<int, string>()
+        {
+            {1, "Mango"},
+            {2, "Banana"},
+            {3, "Orange"}
+        };
+
+// 6. Add a new entry
+fruitDict.Add(4, "Pineapple");
+
+// 7. Print all key-value pairs
+Console.WriteLine("\nFruit Dictionary:");
+foreach (var item in fruitDict)
+{
+    Console.WriteLine($"ID: {item.Key}, Fruit: {item.Value}");
+}
